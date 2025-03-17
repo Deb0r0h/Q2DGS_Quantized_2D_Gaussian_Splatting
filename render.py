af390ff819618439d800d155cfa4c616edf595dd
flexsight <gabriel.taormina@protonmail.com>
2025-03-17 13:09:08 +0100

    add sub

--- render.py
+++ render.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     
     train_dir = os.path.join(args.model_path, 'train', "ours_{}".format(scene.loaded_iter))
     test_dir = os.path.join(args.model_path, 'test', "ours_{}".format(scene.loaded_iter))
-    gaussExtractor = GaussianExtractor(gaussians, render, pipe, bg_color=bg_color)    
+    gaussExtractor = GaussianExtractor(gaussians, render, pipe, bg_color=bg_color)
     
     if not args.skip_train:
         print("export training images ...")
